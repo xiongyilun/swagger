a9db9cd4b753bf350fe15d3d27c22b6bc1d0a638
xiongyilun <xiongyilun@qq.com>
2019-02-20 14:17:40 +0800

    Swagger2和Restful风格的API设计。以及SpringBootTest测试

---
+++ src/test/java/com/example/swagger/SwaggerApplicationTests.java
@@ -0,0 +1,98 @@
+package com.example.swagger;
+
+import com.example.swagger.Controller.UserController;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class SwaggerApplicationTests {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void testUserController() throws Exception {
+        // 测试UserController
+        RequestBuilder request = null;
+
+        // 1、get查一下user列表，应该为空
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+
+        log.info("测试列表为空,成功");
+        // 2、post提交一个user
+        request = post("/users/")
+                .param("id", "1")
+                .param("name", "测试大师")
+                .param("age", "20");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        log.info("增加一条数据：成功!");
+
+        // 3、get获取user列表，应该有刚才插入的数据
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"age\":20}]")));
+
+        log.info("获取到添加的数据：成功!");
+
+        // 4、put修改id为1的user
+        request = put("/users/1")
+                .param("name", "测试终极大师")
+                .param("age", "30");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        log.info("修改数据：成功!");
+
+        // 5、get一个id为1的user
+        request = get("/users/1");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"age\":30}")));
+
+        log.info("修改id为1的数据数据：成功!");
+
+        // 6、del删除id为1的user
+        request = delete("/users/1");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        log.info("删除id为1的数据数据：成功!");
+
+        // 7、get查一下user列表，应该为空
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+
+        log.info("测试列表为空,成功");
+
+    }
+
+}
